c1590b8694f041b92d1c2c21e3aa1c1835edbe5b
vedansh-mishra555 <vedanshmishra333@gmail.com>
2026-09-19 22:55:34 +0530

    variables

---
+++ question1.java
@@ -0,0 +1,8 @@
+public class question1 {
+    public static void main(String[]args){
+System.out.println("*");
+System.out.println("**");
+System.out.println("***");
+System.out.println("****");
+    }
+}
---
+++ variables.java
@@ -0,0 +1,48 @@
+public class variables {
+    public static void main(String[]args){
+System.out.println("Hello World"); 
+System.out.println("Hello World With java"); 
+System.out.println("I AM LEARNING JAVA FROM \nAPNI KAKSHA");
+int a=10;
+int b=20;
+String name="VEDANSH";
+System.out.println(a);
+System.out.println(b);
+System.out.println(name);  
+b=30;
+System.out.println(b);     
+
+
+int sum = a+b;
+int sub = a-b;
+int mul = a*b;
+int div = a/b;
+System.out.println(sum);
+System.out.println(sub);
+System.out.println(mul);
+System.out.println(div);
+    }
+
+}
+
+
+
+/*
+ This is a simple Java program that prints "Hello World" and
+  "Hello World With java"
+  to the console.
+THREE WAYS TO PRINT THE OUTPUT IN JAVA
+println() - prints the output with a new line at the end
+print() - prints the output without a new line at the end
+printf() - prints the output with a specified format
+"\n"--prints the output with a new line at the end
+*/
+
+/*
+What is variables?
+Variables are containers for storing data values.
+*/
+/*
+We can change the value of a variable at any time during the 
+execution of a program.
+*/
